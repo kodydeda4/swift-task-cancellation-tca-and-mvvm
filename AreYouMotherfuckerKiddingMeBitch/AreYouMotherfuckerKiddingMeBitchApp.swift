95011f49e94a97635a45036be7af4cdca89edc2e
Kody Deda <kodydeda4@gmail.com>
2024-12-06 17:31:35 -0500

    Initial Commit

---
+++ AreYouMotherfuckerKiddingMeBitch/AreYouMotherfuckerKiddingMeBitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreYouMotherfuckerKiddingMeBitchApp.swift
+//  AreYouMotherfuckerKiddingMeBitch
+//
+//  Created by Kody Deda on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AreYouMotherfuckerKiddingMeBitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AreYouMotherfuckerKiddingMeBitchTests/AreYouMotherfuckerKiddingMeBitchTests.swift
@@ -0,0 +1,17 @@
+//
+//  AreYouMotherfuckerKiddingMeBitchTests.swift
+//  AreYouMotherfuckerKiddingMeBitchTests
+//
+//  Created by Kody Deda on 12/6/24.
+//
+
+import Testing
+@testable import AreYouMotherfuckerKiddingMeBitch
+
+struct AreYouMotherfuckerKiddingMeBitchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
